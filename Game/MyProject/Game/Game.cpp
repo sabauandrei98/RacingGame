3cbd017ca12f7ce92837fad873a107fe221a770e
Andrei Sabu - (p) <andreis4@andreis4-mbp.local>
2019-04-02 12:13:19 +0300

    Project changes

---
+++ Game/MyProject/Game/Game.cpp
@@ -0,0 +1,13 @@
+//
+//  Game.cpp
+//  Game
+//
+//  Created by Andrei Sabu - (p) on 4/2/19.
+//
+
+#include "Game.hpp"
+
+int main()
+{
+    return 0;
+}
---
+++ Game/MyProject/Game/Game.hpp
@@ -0,0 +1,13 @@
+//
+//  Game.hpp
+//  Game
+//
+//  Created by Andrei Sabu - (p) on 4/2/19.
+//
+
+#ifndef Game_hpp
+#define Game_hpp
+
+#include <stdio.h>
+
+#endif /* Game_hpp */
