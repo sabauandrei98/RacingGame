9f16e721a07e3df3961feef2b5764067c24c85a1
Tamas Both - (p) <tamasb@tamasb-mbp.local>
2019-04-05 15:17:21 +0300

    SceneGraph added

---
+++ Game/MyProject/Game/SceneManagement/SceneGraph.cpp
@@ -0,0 +1,8 @@
+//
+//  SceneGraph.cpp
+//  Game
+//
+//  Created by Tamas Both - (p) on 05/04/2019.
+//
+
+#include "SceneGraph.hpp"
---
+++ Game/MyProject/Game/SceneManagement/SceneGraph.hpp
@@ -0,0 +1,13 @@
+//
+//  SceneGraph.hpp
+//  Game
+//
+//  Created by Tamas Both - (p) on 05/04/2019.
+//
+
+#ifndef SceneGraph_hpp
+#define SceneGraph_hpp
+
+#include <stdio.h>
+
+#endif /* SceneGraph_hpp */
