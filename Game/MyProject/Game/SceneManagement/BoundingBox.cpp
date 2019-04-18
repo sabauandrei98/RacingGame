0b8a448ce1955ef82fc97e808b3e6320e0c7833c
Both Tamás <tsboth@gmailc.com>
2019-04-18 15:33:33 +0300

    bounding box started

---
+++ Game/MyProject/Game/SceneManagement/BoundingBox.cpp
@@ -0,0 +1,8 @@
+//
+//  BoundingBox.cpp
+//  RacingGame
+//
+//  Created by Tamas Both - (p) on 18/04/2019.
+//
+
+#include "BoundingBox.hpp"
---
+++ Game/MyProject/Game/SceneManagement/BoundingBox.hpp
@@ -0,0 +1,13 @@
+//
+//  BoundingBox.hpp
+//  RacingGame
+//
+//  Created by Tamas Both - (p) on 18/04/2019.
+//
+
+#ifndef BoundingBox_hpp
+#define BoundingBox_hpp
+
+#include <stdio.h>
+
+#endif /* BoundingBox_hpp */
