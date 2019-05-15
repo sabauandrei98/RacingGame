c5dba3e20a1f00cbb3655c2dd196cf5fc0692ac0
Both Tamás <tsboth@gmailc.com>
2019-05-15 14:56:47 +0300

    weird stuff

---
+++ Game/MyProject/DeferredRendering/DeferredRenderer.cpp
@@ -0,0 +1,8 @@
+//
+//  DeferredRenderer.cpp
+//  RacingGame
+//
+//  Created by Tamas Both - (p) on 14/05/2019.
+//
+
+#include "DeferredRenderer.hpp"
---
+++ Game/MyProject/DeferredRendering/DeferredRenderer.hpp
@@ -0,0 +1,13 @@
+//
+//  DeferredRenderer.hpp
+//  RacingGame
+//
+//  Created by Tamas Both - (p) on 14/05/2019.
+//
+
+#ifndef DeferredRenderer_hpp
+#define DeferredRenderer_hpp
+
+#include <stdio.h>
+
+#endif /* DeferredRenderer_hpp */
